7ed6522c301f061b13da10a0b3aa37a055325d5f
WhiteGray <WhiteGray-Black@yandex.ru>
2020-11-18 02:56:19 +0300

    HomeWork4

--- src/main/java/ru/geekbrains/persist/ToDoRepository.java
+++ src/main/java/ru/geekbrains/persist/ToDoRepository.java
@@ -1,67 +1,84 @@
 package ru.geekbrains.persist;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.ServletContext;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+@Named
+@ApplicationScoped
 public class ToDoRepository {
 
-    private final Connection conn;
+    @Inject
+    private ServletContext context;
 
-    public ToDoRepository(Connection conn) throws SQLException {
-        this.conn = conn;
+    private Connection conn;
+
+    @PostConstruct
+    public void init() throws SQLException {
+        this.conn = (Connection) context.getAttribute("jdbcConnection");
         createTableIfNotExists(conn);
     }
 
     public void insert(ToDo toDo) throws SQLException {
         try (PreparedStatement stmt = conn.prepareStatement(
-                "insert into todos(description, targetDate) values (?, ?);")) {
-            stmt.setString(1, toDo.getDescription());
+                "insert into productes (name, price, quantity, description) values (?, ?, ?, ?);")) {
+
+            stmt.setLong(1, toDo.getId());
+            stmt.setString(2, toDo.getName());
+            stmt.setFloat(3, toDo.getPrice());
+            stmt.setLong(4, toDo.getQuantity());
+            stmt.setString(5, toDo.getDescription());
             stmt.execute();
         }
     }
 
     public void update(ToDo toDo) throws SQLException {
         try (PreparedStatement stmt = conn.prepareStatement(
-                "update todos set description = ?, targetDate = ? where id = ?;")) {
-            stmt.setString(1, toDo.getDescription());
-            stmt.setInt(2, toDo.getQuantity());
-            stmt.setInt(3, toDo.getPrice());
-            stmt.setString(4, toDo.getItem());
+                "update productes set name = ?, price = ?, quantity = ?, description = ? where id = ?;")) {
+
+                stmt.setString(1, toDo.getDescription());
+            stmt.setLong(2, toDo.getQuantity());
+            stmt.setFloat(3, toDo.getPrice());
+            stmt.setString(4, toDo.getName());
             stmt.setLong(5, toDo.getId());
             stmt.execute();
         }
     }
 
-    public void delete(long id) throws SQLException {
+    public void delete(Long id) throws SQLException {
         try (PreparedStatement stmt = conn.prepareStatement(
-                "delete from todos where id = ?;")) {
+                "delete from productes where id = ?;")) {
             stmt.setLong(1, id);
             stmt.execute();
         }
     }
 
-    public ToDo findById(long id) throws SQLException {
+    public ToDo findById(Long id) throws SQLException {
         try (PreparedStatement stmt = conn.prepareStatement(
-                "select id, description, targetDate from todos where id = ?")) {
+                "select id, name, price, quantity, description from productes where id = ?")) {
             stmt.setLong(1, id);
             ResultSet rs = stmt.executeQuery();
 
             if (rs.next()) {
-                 return new ToDo(rs.getLong(1), rs.getString(2), rs.getInt(3), rs.getInt(4), rs.getString(5));
+                return new ToDo(rs.getLong(1), rs.getString(2), rs.getFloat(3), rs.getLong(4), rs.getString(5));
             }
         }
-        return new ToDo(-1L, "", 0, 0, "");
+        return new ToDo(-1L, "", 0.0f, -1L, "");
     }
 
     public List<ToDo> findAll() throws SQLException {
         List<ToDo> res = new ArrayList<>();
         try (Statement stmt = conn.createStatement()) {
-            ResultSet rs = stmt.executeQuery("select id, item, price, quantity, description, from catalogs");
+            ResultSet rs = stmt.executeQuery("select id, name, price, quantity, description from productes");
 
             while (rs.next()) {
-                res.add(new ToDo(rs.getLong(1), rs.getString(2), rs.getInt(3), rs.getInt(4), rs.getString(5)));
+                res.add(new ToDo(rs.getLong(1), rs.getString(2), rs.getFloat(3), rs.getLong(4), rs.getString(5)));
             }
         }
         return res;
@@ -69,11 +86,14 @@ public class ToDoRepository {
 
     private void createTableIfNotExists(Connection conn) throws SQLException {
         try (Statement stmt = conn.createStatement()) {
-            stmt.execute("create table if not exists todos (\n" +
+            stmt.execute("create table if not exists productes (\n" +
                     "\tid int auto_increment primary key,\n" +
-                    "    description varchar(25),\n" +
-                    "    targetDate date \n" +
+                    "    name varchar(25),\n" +
+                    "    price float, \n" +
+                    "    quantity long,\n" +
+                    "    description varchar(25)\n" +
                     ");");
-        }
+
+            }
     }
 }
