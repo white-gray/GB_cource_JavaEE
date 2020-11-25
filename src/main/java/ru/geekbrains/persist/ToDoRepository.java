d5cb8f0a893bcd38658d42f9a1b471b8bce416d7
WhiteGray <WhiteGray-Black@yandex.ru>
2020-11-25 18:49:37 +0300

    end

---
+++ src/main/java/ru/geekbrains/persist/ToDoRepository.java
@@ -0,0 +1,47 @@
+package ru.geekbrains.persist;
+
+
+import javax.enterprise.context.ApplicationScoped;
+
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import javax.transaction.Transactional;
+
+import java.util.List;
+
+@Named
+@ApplicationScoped
+public class ToDoRepository {
+
+    @PersistenceContext(unitName = "ds")
+    private EntityManager em;
+
+    @Transactional
+    public void insert(ToDo toDo) {
+        em.persist(toDo);
+    }
+
+    @Transactional
+    public void update(ToDo toDo) {
+        em.merge(toDo);
+    }
+
+    @Transactional
+    public void delete(long id) {
+        ToDo toDo = em.find(ToDo.class, id);
+        if (toDo != null) {
+            em.remove(toDo);
+        }
+    }
+
+    public ToDo findById(long id) {
+        return em.find(ToDo.class, id);
+    }
+
+    public List<ToDo> findAll() {
+        return em.createQuery("from ToDo t", ToDo.class)
+                .getResultList();
+    }
+}
